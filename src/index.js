47c940690c600c18f7d4636589856a609455c4d2
stix2you <stix2you@gmail.com>
2024-02-23 22:23:32 -0600

    tidy up basic files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootie = document.getElementById('root');
 
+const root = ReactDOM.createRoot(rootie);
+root.render(<App />);
